657d06cb4c28a078196037c7ce8895adbdeab92e
flames929 <59063635+flames929@users.noreply.github.com>
2019-12-27 02:48:56 -0800

    Create code.js

---
+++ code.js
@@ -0,0 +1,8 @@
+document.write("<h1>Hello world</h1>");
+document.write("<hr />");
+document.write("<p>This a javascript tutorial</p>");
+var name = window.prompt("Enter your name : ");
+alert("This is an alert "+ name);
+
+
+document.write("<h1>Hello world "+ name + " wow");
