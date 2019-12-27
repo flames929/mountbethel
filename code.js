f7844a6aabd8e573f69c374cf33cd3cd5d030a7d
flames929 <59063635+flames929@users.noreply.github.com>
2019-12-27 11:44:23 -0800

    Update code.js

--- code.js
+++ code.js
@@ -2,5 +2,5 @@ var name = window.prompt("Enter your name : ");
 alert("This is an alert "+ name);
 
 
-document.write("<h1>Hello world "+ name + " wow");
+document.write("Dear "+ name + " welcome to mount bethel");
  
