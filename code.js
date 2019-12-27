73b4e25cd14ccb626a09c4af6cec66e0395314d1
flames929 <59063635+flames929@users.noreply.github.com>
2019-12-27 11:43:14 -0800

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,3 @@
-document.write("<h1>Hello world</h1>");
-document.write("<hr />");
-document.write("<p>This a javascript tutorial</p>");
 var name = window.prompt("Enter your name : ");
 alert("This is an alert "+ name);
 
